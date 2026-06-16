62e4daa7f080a09c7c87e7fe1ba4650bdaf63b0a
yundali <yunli1618@gmail.com>
2026-06-15 20:52:28 -0400

    Working Sweep

--- src/soarm_controller/launch/joint_test.launch.py
+++ src/soarm_controller/launch/joint_test.launch.py
@@ -1,31 +1,45 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, LaunchConfiguration
 import os
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    #Retrieve file path of URDF file
+    soarm_description_dir = get_package_share_directory("soarm_description")
+
+    is_sim_arg = DeclareLaunchArgument(
+        name="is_sim",
+        #CHANGE HERE
+        default_value="false",
+        description="Toggle between simulation (True) and real hardware (False)"
+    )
+
+    is_sim = LaunchConfiguration("is_sim")
+
     model_arg = DeclareLaunchArgument(
         name="model", 
-        default_value=os.path.join(get_package_share_directory("soarm_description"), "urdf", "soarm.urdf.xacro"),
+        default_value=os.path.join(soarm_description_dir, "urdf", "soarm.urdf.xacro"),
         description="Absolute path to robot URDF file"
-        )
+    )
     
-    #Need to convert xacro to readable URDF, using path defined earlier DeclareLaunchArgument(name="model"), Command specifies conversion
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")])) #SPACE AFTER XACRO IS IMPORTANT
+    robot_description = ParameterValue(
+        Command([
+            "xacro ", 
+            LaunchConfiguration("model"),
+            " is_sim:=", is_sim
+        ]),
+        value_type=str
+    )
 
-
-    #Typical Syntax for starting Nodes, these are most important
     robot_state_pub = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{
-            'robot_description': robot_description, # Use the variable name here!
-            'use_sim_time': True
+            'robot_description': robot_description,
+            'use_sim_time': is_sim
         }]
     )
 
@@ -34,13 +48,12 @@ def generate_launch_description():
         executable="joint_tester",
         parameters=[{
             'robot_description': robot_description,
-            'use_sim_time' : True
+            'use_sim_time': is_sim
         }]
     )
 
-
-    #LaunchDescription is a list of instructions to execute when launching file. Define objects, and stick into list of args
     return LaunchDescription([
+        is_sim_arg,
         model_arg,
         robot_state_pub,
         joint_tester
--- src/soarm_scripts/src/joint_tester.cpp
+++ src/soarm_scripts/src/joint_tester.cpp
@@ -14,6 +14,8 @@
 using namespace std::chrono_literals;
 using namespace std::placeholders;
 
+#define RAD_CONV 0.001533979
+
 class JointTester : public rclcpp::Node
 {
     public:     
@@ -22,9 +24,9 @@ class JointTester : public rclcpp::Node
             this->declare_parameter<std::string>("robot_description", "");
 
             pub_ = create_publisher<trajectory_msgs::msg::JointTrajectory>("/arm_controller/joint_trajectory", 1); 
-            timer_ = create_wall_timer(5s, std::bind(&JointTester::timerCallback, this)); 
+            timer_ = create_wall_timer(52s, std::bind(&JointTester::timerCallback, this)); 
 
-            RCLCPP_INFO_STREAM(get_logger(), "Publishing at .2 hz"); 
+            RCLCPP_INFO_STREAM(get_logger(), "Publishing every 35s"); 
 
             initKDL();
         }
@@ -37,14 +39,25 @@ class JointTester : public rclcpp::Node
         std::random_device rd_;
         std::default_random_engine re_{rd_()};
 
-        std::vector<std::string> joint_names_ = {
+        const std::vector<std::string> joint_names_ = {
                 "shoulder_pan",
                 "shoulder_lift",
                 "elbow_flex",
                 "wrist_flex",
                 "wrist_roll",
                 // "gripper_joint"
-            };
+        };
+
+        const std::unordered_map<std::string, double> joint_homes_ = {
+                {"shoulder_pan", 2500 * RAD_CONV},
+                {"shoulder_lift", 1000 * RAD_CONV},
+                {"elbow_flex", 2500 * RAD_CONV},
+                {"wrist_flex", 1300 * RAD_CONV},
+                {"wrist_roll", 3300 * RAD_CONV},
+                // "gripper_joint"
+        };
+
+        std::map<std::string, std::pair<double, double>> joint_data_;
         std::vector<double> joint_angles_;
         std::vector<KDL::Vector> joint_positions_;
 
@@ -67,6 +80,7 @@ class JointTester : public rclcpp::Node
                             
                 if (model.initString(urdf_string)) {
                     joint_angles_.clear();
+                    joint_data_.clear();
 
                     for (const auto& name : joint_names_){
                         auto joint = model.getJoint(name);
@@ -77,6 +91,8 @@ class JointTester : public rclcpp::Node
                             double random_joint_position = randomJointPosition(lower_limit, upper_limit);
                             joint_angles_.push_back(random_joint_position);
 
+                            joint_data_[name] = {lower_limit, upper_limit};
+
                             RCLCPP_INFO_STREAM(this->get_logger(), name << " limits, upper: " << upper_limit << " lower: " << lower_limit
                                                                 << "\n random joint position: " << random_joint_position);
                         }
@@ -86,6 +102,7 @@ class JointTester : public rclcpp::Node
                     }
                 }
             }
+
             std::stringstream ss;
             ss << "[ ";
             for (const auto& pos : joint_angles_) {
@@ -160,36 +177,15 @@ class JointTester : public rclcpp::Node
         }
 
         void publishTrajectory(){
-            //trajectory_msgs::msg::JointTrajectory example:
-            //   header: {
-            //     stamp: {sec: 0, nanosec: 0},
-            //     frame_id: 'base_link'
-            //   },
-            //   joint_names: ['shoulder_pan'],
-            //   points: [
-            //     {
-            //       positions: [1.0],
-            //       time_from_start: {sec: 2, nanosec: 0}
-            //     }
-            //   ]
-            // }"
-
             if (!joint_angles_.empty()){
                 auto message = trajectory_msgs::msg::JointTrajectory();
-
                 message.header.stamp = this->get_clock()->now();
-                // message.header.stamp = rclcpp::Time(0,0);
-
                 message.header.frame_id = "base_link";
-
-                message.joint_names = joint_names_; //{"shoulder_pan", "shoulder_lift", ...}
+                message.joint_names = joint_names_; 
 
                 auto point = trajectory_msgs::msg::JointTrajectoryPoint();
-
                 point.positions = joint_angles_;
-                // point.velocities = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
                 point.time_from_start = rclcpp::Duration::from_seconds(4.0);
-
                 message.points.push_back(point);
 
                 pub_->publish(message);
@@ -201,9 +197,161 @@ class JointTester : public rclcpp::Node
             }
         }
 
+        void homeJoints(){
+            std::vector<double> home_vec;
+                for (const auto& joint : joint_names_){
+                    if (joint_homes_.find(joint) != joint_homes_.end()){
+                        home_vec.push_back(joint_homes_.at(joint));
+                    } else {
+                        home_vec.push_back(0.0);
+                    }    
+                }
+
+                auto message = trajectory_msgs::msg::JointTrajectory();
+                message.header.stamp = this->get_clock()->now();
+                message.header.frame_id = "base_link";
+                message.joint_names = joint_names_;
+
+                auto point = trajectory_msgs::msg::JointTrajectoryPoint();
+                std::vector<double> command_vec = home_vec; 
+
+                point.positions = command_vec;
+                point.time_from_start = rclcpp::Duration::from_seconds(2.0); 
+                message.points.push_back(point);
+
+                RCLCPP_INFO_STREAM(this->get_logger(), "======Homing=========");
+                pub_->publish(message);
+
+                rclcpp::Rate(0.5).sleep();
+
+
+        }
+
+        void jointSweep(){
+            if (!joint_data_.empty()){
+                std::vector<double> home_vec;
+                for (const auto& joint : joint_names_){
+                    if (joint_homes_.find(joint) != joint_homes_.end()){
+                        home_vec.push_back(joint_homes_.at(joint));
+                    } else {
+                        home_vec.push_back(0.0);
+                    }    
+                }
+
+                for (size_t i = 0; i < joint_names_.size(); ++i){
+                    std::string current_joint = joint_names_[i];
+
+                    if (joint_data_.find(current_joint) == joint_data_.end()) {
+                        RCLCPP_WARN_STREAM(this->get_logger(), "Skipping " << current_joint << ", no limit data.");
+                        continue;
+                    }
+
+                    double lower_limit = joint_data_[current_joint].first;
+                    double upper_limit = joint_data_[current_joint].second;
+
+                    {
+                        auto message = trajectory_msgs::msg::JointTrajectory();
+                        message.header.stamp = this->get_clock()->now();
+                        message.header.frame_id = "base_link";
+                        message.joint_names = joint_names_;
+
+                        auto point = trajectory_msgs::msg::JointTrajectoryPoint();
+                        std::vector<double> command_vec = home_vec; 
+                        command_vec[i] = lower_limit; 
+
+                        point.positions = command_vec;
+                        point.time_from_start = rclcpp::Duration::from_seconds(2.0); 
+                        message.points.push_back(point);
+
+                        RCLCPP_INFO_STREAM(this->get_logger(), "Moving " << current_joint << " to LOWER limit: " << lower_limit);
+                        pub_->publish(message);
+
+                        rclcpp::Rate(0.5).sleep();
+
+                    }
+
+                    homeJoints();
+                    //Publish Upper Bound
+                    {
+                        auto message = trajectory_msgs::msg::JointTrajectory();
+                        message.header.stamp = this->get_clock()->now();
+                        message.header.frame_id = "base_link";
+                        message.joint_names = joint_names_;
+
+                        auto point = trajectory_msgs::msg::JointTrajectoryPoint();
+                        std::vector<double> command_vec = home_vec; 
+                        command_vec[i] = upper_limit; 
+
+                        point.positions = command_vec;
+                        point.time_from_start = rclcpp::Duration::from_seconds(2.0); 
+                        message.points.push_back(point);
+
+                        RCLCPP_INFO_STREAM(this->get_logger(), "Moving " << current_joint << " to UPPER limit: " << upper_limit);
+                        pub_->publish(message);
+
+                        rclcpp::Rate(0.5).sleep();
+
+
+                    }
+
+                    homeJoints();
+
+                }
+            }
+            else{
+                RCLCPP_WARN(this->get_logger(), "No Joint Trajectory Provided or Invalid Trajectory");
+            }
+
+        }
+
+        bool flag_ = true;
+
+        void simpleSweep(){
+            {
+                auto message = trajectory_msgs::msg::JointTrajectory();
+                message.header.stamp = this->get_clock()->now();
+                message.header.frame_id = "base_link";
+                message.joint_names = joint_names_;
+
+                std::vector<double> home_vec;
+                for (const auto& joint : joint_names_){
+                    if (joint_homes_.find(joint) != joint_homes_.end()){
+                        home_vec.push_back(joint_homes_.at(joint));
+                    } else {
+                        home_vec.push_back(0.0);
+                    }    
+                }
+
+                auto point = trajectory_msgs::msg::JointTrajectoryPoint();
+
+                if (flag_){
+                    home_vec[3] = 1200*RAD_CONV;
+                    point.positions = home_vec;
+                    RCLCPP_INFO_STREAM(this->get_logger(), "Sending Command to Position 1200");
+                } else {
+                    home_vec[3] = 3200*RAD_CONV;
+                    point.positions = home_vec;
+                    RCLCPP_INFO_STREAM(this->get_logger(), "Sending Command to Position 3200");
+                }
+
+                point.time_from_start = rclcpp::Duration::from_seconds(2.0); 
+                message.points.push_back(point);
+
+                pub_->publish(message);
+
+                flag_ = !flag_;
+
+            }
+
+        }
+
+        void sleepTimerCallback(){
+            RCLCPP_INFO(this->get_logger(), "...Sleeping...");
+        }
 
         void timerCallback(){
             pullJointLimits();
+            jointSweep();
 
             if(validateTrajectory()){
                 publishTrajectory();
@@ -211,6 +359,7 @@ class JointTester : public rclcpp::Node
                 RCLCPP_INFO(this->get_logger(), "Waiting for next Trajectory");
             }
         }
+
         
 };
 
